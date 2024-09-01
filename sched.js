31f187c5b5d4ee2e3da850aa21200e19e1607c9f
FlexaGroup <162819959+FlexaGroup@users.noreply.github.com>
2024-09-01 14:23:42 +0100

    sched.js update

--- sched.js
+++ sched.js
@@ -93,7 +93,7 @@ document.addEventListener('DOMContentLoaded', function () {
             const djName = document.getElementById('dj-name').value;
 
             // Send data to Netlify serverless function
-            fetch('/netlify/functions/add-schedule', {
+            fetch('/netlify/functions/add-schedule.js', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ showDate, showTime, djName })
